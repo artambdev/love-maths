35e396b6b7e75689a1765a08ea3780665ab1fd93
Arthur Ambalov <arthurambalov@gmail.com>
2024-02-18 15:24:08 +0000

    Add logic for verifying correct answer and rerunning game

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -36,8 +36,22 @@ function runGame(gameType) {
     }
 }
 
+/**
+ * Check the correct answer against the first element in the
+ * return calculateCorrectAnswer() array
+ */
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect) {
+        alert("Correct! Good job!");
+    } else {
+        alert(`Your answer was ${userAnswer}. The correct answer was ${calculatedAnswer[0]}.`);
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
 /**
